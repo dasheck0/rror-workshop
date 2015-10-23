24cc0190070e143696de3d02b197f37c1373fc85
Stefan Neidig <s.neidig@Daniels-iMac.fritz.box>
2015-10-23 08:41:57 +0200

    Add file for loops

---
+++ ruby/2_2_loops.rb
@@ -0,0 +1,39 @@
+# simple for loop
+for i in [1,2,3,4,5,6]
+  puts "This is #{i}"
+end
+
+# we can use ranges to. in for loops the basic range step is 1
+for i in (1..10)
+  puts "The range value is #{i}"
+end
+
+# simple while loop
+condition = true
+while condition
+  if condition
+    puts "We're now leaving the while loop"
+    condition = false
+  end
+end
+
+puts "Left while loop"
+
+condition = true
+until !condition do
+  if condition
+    puts "We're now leaving the until loop"
+    condition = false
+  end
+end
+
+puts "Left until loop"
+
+=begin
+
+  We can also make while and until loops inline:
+
+    puts "Running in while loop" while condition
+    puts "Running in until loop" until condition
+
+=end
